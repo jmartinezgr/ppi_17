e452407588a31ac3032017f5c964405f07d11f89
Juan <josemargri3@gmail.com>
2023-10-19 20:41:29 -0500

    Se instancias las vistas y templates referentes a la funcionalidades que se pueden realizar sin haberse logueado a la pagina

--- conductor_amigo/conductor_amigo/settings.py
+++ conductor_amigo/conductor_amigo/settings.py
@@ -116,8 +116,13 @@ import os
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = '/static/'
-STATICFILES_DIRS = [os.path.join(BASE_DIR,'static')]
+# settings.py
+
+STATIC_URL = 'static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+    ]
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
--- conductor_amigo/mainapp/urls.py
+++ conductor_amigo/mainapp/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path('home/', views.home, name='home'),
+    path('logout/', views.logout_view, name='logout_view'),
+    path('nosotros/',views.nosotros,name="nosotros")
 ]
