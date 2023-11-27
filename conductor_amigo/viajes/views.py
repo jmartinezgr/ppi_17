fd0d30e5719413c80f938a7035ef5c706463abfa
Juan <josemargri3@gmail.com>
2023-11-27 00:46:26 -0500

    Eliminando vista innecesaria

--- conductor_amigo/viajes/views.py
+++ conductor_amigo/viajes/views.py
@@ -17,72 +17,9 @@ import folium
 import re
 ####Librerias de la app
 from .models import Viaje
-from .forms import ViajesForm,CoordenadaForm
+from .forms import ViajesForm
 ####Librerias de otras apps
 from usuarios.models import Usuario
-
-@login_required
-def ingresar_coordenada(request):
-    """
-    Vista que permite a un usuario ingresar coordenadas y calcula la distancia y tiempo de viaje entre ellas.
-
-    Esta vista utiliza un formulario (CoordenadaForm) para obtener las coordenadas del usuario
-    y realiza cálculos utilizando las funciones calcular_punto_medio y calcular_distancia_tiempo.
-    El resultado incluye información sobre la distancia y tiempo de viaje, así como un mapa interactivo.
-
-    Args:
-        request (HttpRequest): El objeto HttpRequest que representa la solicitud del usuario.
-
-    Returns:
-        HttpResponse: Una respuesta HTTP que renderiza la plantilla 'conductores/rutas_similares.html'
-                      con el formulario, el resultado y el mapa.
-    """
-    # Inicializa la variable que contendrá el resultado
-    data_ret = None
-    mapa_html = None  # Definir mapa_html por defecto
-
-    # Inicializa el mapa de Folium fuera del bloque if
-    mapa = folium.Map(location=[0, 0], zoom_start=12)
-
-    if request.method == 'POST':
-        # Crea un formulario a partir de los datos POST
-        form = CoordenadaForm(request.POST)
-
-        if form.is_valid():
-            # Procesa los datos si el formulario es válido
-
-            selected_option = form.cleaned_data.get('starting_place_type')
-
-            # Busca las coordenadas correspondientes a la opción seleccionada
-
-            selected_option_2 = form.cleaned_data.get('ending_place_type')
-
-            # convertir selected_option a coordenadas
-            start_coord = eval(selected_option)
-            end_coord = eval(selected_option_2)
-
-            punto_medio = calcular_punto_medio(start_coord[0], start_coord[1], end_coord[0], end_coord[1])
-
-            # Calcula la distancia usando las coordenadas del usuario y las coordenadas temporales
-            data_ret = calcular_distancia_tiempo(start_coord, end_coord)
-
-            # Crear el mapa de Folium
-            mapa = folium.Map(location=punto_medio, zoom_start=15)
-            folium.Marker([start_coord[0], start_coord[1]], popup='Coordenada 1').add_to(mapa)
-            folium.Marker([end_coord[0], end_coord[1]], popup='Coordenada 2').add_to(mapa)
-
-            # Convertir el mapa de Folium a HTML
-            mapa_html = mapa._repr_html_()
-        else:
-            # Si el formulario no es válido, muestra un mensaje de error
-            messages.error(request, "Por favor, selecciona una opción válida.")
-    else:
-        # Si no es una solicitud POST, crea un formulario vacío
-        form = CoordenadaForm()
-
-    # Renderiza la plantilla 'rutas_similares.html' con el formulario y el resultado
-    return render(request, 'conductores/rutas_similares.html', {'form': form, 'data_ret': data_ret, 'mapa': mapa_html })
-
 
 @login_required
 def lista_viajes(request):
